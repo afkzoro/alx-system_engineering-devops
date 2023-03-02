9f5a90b6b6ab436097d523e160439e1de930f52f
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-03-02 21:12:42 +0100

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(link, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
