c49a17fd105933f53b0c87268d049afe63e543ba
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-03-02 21:40:02 +0100

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """ Function to get number of subs on a subreddit"""
     link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/afkzoro)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(link, headers=headers, allow_redirects=False)
     if response.status_code == 404:
