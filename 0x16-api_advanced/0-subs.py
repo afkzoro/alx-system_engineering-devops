b3b141535fcf4f7766bd67f586ec85cf0d6534ad
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-03-02 21:20:33 +0100

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,7 @@ def number_of_subscribers(subreddit):
     """ Function to get number of subs on a subreddit"""
     link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": """Mozilla/5.0 (Windows NT 10.0; Win64; x64) 
-        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"""
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
     }
     response = requests.get(link, headers=headers, allow_redirects=False)
     if response.status_code == 404:
